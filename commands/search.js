05a9910b430babe80a9c0910a0581acbe46a5691
giokalm <giorgoskalmoukis@outlook.com>
2019-02-17 06:36:30 +0200

    Add string manipulate functions

--- commands/search.js
+++ commands/search.js
@@ -1,20 +1,10 @@
 var unirest = require('unirest');// rapid api sdk
 const {Attachment} = require('discord.js');
+var string_tools = require('../functions/manipulate_string');
 module.exports = message => {
     //todo: Optional Parameters
-    // Split string by whitespace character
-    const splitString = message.content.split(" ");
-    var name = "";
-    for (let index = 1; index < splitString.length; index++) {
-        name += splitString[index]+" ";
-    }
-    unirest.get(`https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/search/${name}`)
-        .header("X-RapidAPI-Key", "c0b422b7d9mshcdd1a3320f7f68bp1762dejsnd3329ec2021c").end(function (result) {
-            console.log(result.status, result.headers, result.body);
-            var res = "";
-            result.body.forEach(element => {
-                res += element.name+"\n";
-            });
-            message.channel.send(res);
+    unirest.get(`https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/search/${string_tools.requested_name(message)}`)
+        .header("X-RapidAPI-Key", "c0b422b7d9mshcdd1a3320f7f68bp1762dejsnd3329ec2021c").end(function (result) {        
+            message.channel.send(string_tools.search_results(result.body));
         });
 };
---
+++ functions/manipulate_string.js
@@ -0,0 +1,37 @@
+var pluralize = require('pluralize');
+module.exports = {
+    card_info: function (json) {
+        var info = "Card info";
+        info += (( typeof json.name !== 'undefined') ? `\nName: ${json.name}` : `` );
+        info += (( typeof json.type !== 'undefined') ? `\nType: ${json.type}` : `` );
+        info += (( typeof json.cardSet !== 'undefined') ? `\nSet: ${json.cardSet}` : `` );
+        info += (( typeof json.faction !== 'undefined') ? `\nFaction: ${json.faction}` : `` );
+        info += (( typeof json.rarity !== 'undefined') ? `\nRarity: ${json.rarity}` : `` );
+        info += (( typeof json.cost !== 'undefined') ? `\nCost: ${json.cost}` : `` );
+        info += (( typeof json.attack !== 'undefined') ? `\nAttack: ${json.attack}` : `` );
+        info += (( typeof json.health !== 'undefined') ? `\nHealth: ${json.health}` : `` );
+        info += (( typeof json.race !== 'undefined') ? `\nRace: ${json.race}` : `` );
+        info += (( typeof json.playerClass !== 'undefined') ? `\nPlayer: ${json.playerClass}` : `` );
+        info += (( typeof json.elite !== 'undefined') ? `\nElite: ${json.elite}` : `` );
+        info += (( typeof json.collectible !== 'undefined') ? `\nCollectible: ${json.collectible}` : `` );
+        info += (( typeof json.flavor !== 'undefined') ? `\nFlavor: ${json.flavor}` : `` );
+        info += (( typeof json.artist !== 'undefined') ? `\nArtist: ${json.artist}` : `` );
+        return info;
+    },
+    search_results: function (json) {
+        var results = `I found ${pluralize('card', json.length,true)}:`;
+        json.forEach(element => {
+            results += `\n${element.name}`;
+        });
+        return results;
+    },
+    requested_name: function(command){
+        // Split string by whitespace character
+        const split_string = command.content.split(" ");
+        var name = "";
+        for (let index = 1; index < split_string.length; index++) {
+            name += split_string[index]+" ";
+        }
+        return name;
+    }
+};
